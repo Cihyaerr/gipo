52cfd8e8daba8b37b958cc61d1fe148c0bad66c4
Cihyaerr <92590655+Cihyaerr@users.noreply.github.com>
2021-10-16 01:02:28 +0700

    Create lypo.sh

---
+++ lypo.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u DK6MFYcQV2gVEku8PHfHiqtAWDPfbHnjf8 -p c=DGB,zap=GXX -t 8
